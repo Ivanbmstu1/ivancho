c79603337739a83ca121bbca90c26b301e463c0c
Ivanbmstu1 <sanfirov.vania2106@gmail.com>
2025-05-20 00:24:14 +0300

    hw_2

---
+++ test_sorts.h
@@ -0,0 +1,18 @@
+#ifndef TEST_SORTS_H
+#define TEST_SORTS_H
+
+#include "sorts.h"
+
+void test_BubbleSort();
+void test_InsertionSort();
+void test_SelectionSort();
+void test_ShellSort();
+void test_CountSort();
+void test_QuickSort();
+void test_QuickSortMod();
+void test_MergeSort();
+void test_HeapSort();
+void test_RadixSort();
+void test_TimSort();
+
+#endif // TEST_SORTS_H
